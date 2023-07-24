57c70b3992de2dbbf0a76c123f2611a86153e8aa
OtakuZhang <zhangyk@shanghaitech.edu.cn>
2023-07-24 16:14:38 -0400

    added rotation and direction to main charactor and bullet

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -9,12 +9,12 @@ public class Fire : MonoBehaviour
     float lastFire = 0.0f;
     CharacterController controller;
 
-    void SpawnBullet()
+    void SpawnBullet(float angle, Vector3 direction)
     {
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, angle));
         
         Bullet bulletScript = bullet.GetComponent<Bullet>();
-        bulletScript.direction = new Vector3(0.0f, 1.0f, 0.0f);
+        bulletScript.direction = direction;
     }
     
     // Start is called before the first frame update
@@ -28,9 +28,28 @@ public class Fire : MonoBehaviour
     {
         lastFire += Time.deltaTime;
 
+        
+
+        // Always face mouse cursor
+        // Get the mouse position in world coordinates
+        Vector3 LookAtPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Make sure the z position is the same as the sprite
+        LookAtPosition.z = transform.position.z;
+
+        // Calculate the direction from the sprite to the mouse
+        Vector3 direction = LookAtPosition - transform.position;
+
+        // Calculate the angle of the direction vector,
+        // subtract 90 degrees because the sprite is facing up
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+        // Set the z rotation of the sprite to this angle
+        //transform.rotation = Quaternion.Euler(0, 0, angle);
+
         if (Input.GetButton("Fire1") && lastFire >= timeBetweenFires)
         {
-            SpawnBullet();
+            SpawnBullet(angle, direction);
             lastFire = 0.0f;
         }
     }
---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    public GameObject Prefab2LookAt = null; // null = look at mouse cursor
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Prefab2LookAt == null)
+        {
+            // Always face mouse cursor
+            // Get the mouse position in world coordinates
+            Vector3 LookAtPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Make sure the z position is the same as the sprite
+            LookAtPosition.z = transform.position.z;
+
+            // Calculate the direction from the sprite to the mouse
+            Vector3 direction = LookAtPosition - transform.position;
+
+            // Calculate the angle of the direction vector,
+            // subtract 90 degrees because the sprite is facing up
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+            // Set the z rotation of the sprite to this angle
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
