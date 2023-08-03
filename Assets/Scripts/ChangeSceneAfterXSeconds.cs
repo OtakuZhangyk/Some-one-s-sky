da47a4cea8d54a34ffd50726676e377d8adad6ce
OtakuZhang <zhangyk@shanghaitech.edu.cn>
2023-08-02 22:03:17 -0400

    connected scenes

---
+++ Assets/Scripts/ChangeSceneAfterXSeconds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAfterXSeconds : MonoBehaviour
+{
+    public float seconds;
+    public string sceneName;
+
+    private bool triggered = false;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= seconds && !triggered)
+        {
+            triggered = true;
+            SceneTransitionManager.Instance.FadeAndLoadScene(sceneName);
+        }
+    }
+}
